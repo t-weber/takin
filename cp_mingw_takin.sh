a5f3672d9a115210241c66a8ad5fde73ff092de7
Tobias Weber <tobias.weber@frm2.tum.de>
2017-02-25 20:42:46 +0100

    makelists update

---
+++ cp_mingw_takin.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+# installation directory
+INSTDIR=~/.wine/drive_c/takin
+
+
+# main program
+mkdir ${INSTDIR}
+cp -v bin/takin.exe 	${INSTDIR}/
+cp -v COPYING			${INSTDIR}/
+cp -v LICENSES			${INSTDIR}/
+cp -v LITERATURE		${INSTDIR}/
+cp -v AUTHORS			${INSTDIR}/
+
+
+
+# resources
+mkdir ${INSTDIR}/res
+cp -rv res/* 	${INSTDIR}/res/
+gunzip -v ${INSTDIR}/res/data/*
+
+
+
+# libraries
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libstdc++-6.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libwinpthread-1.dll	${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libpng16-16.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgcc_s_sjlj-1.dll	${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgcc_s_seh-1.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libbz2-1.dll			${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/zlib1.dll				${INSTDIR}/
+
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_regex-mt.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_system-mt.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_iostreams-mt.dll	${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_filesystem-mt.dll	${INSTDIR}/
+
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/QtCore4.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/QtGui4.dll			${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/QtOpenGL4.dll		${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/QtSvg4.dll			${INSTDIR}/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/qwt.dll			${INSTDIR}/
+
+
+
+# qt plugins
+mkdir -p ${INSTDIR}/lib/plugins/iconengines/
+mkdir -p ${INSTDIR}/lib/plugins/imageformats
+
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/iconengines/qsvgicon4.dll	${INSTDIR}/lib/plugins/iconengines/
+cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/imageformats/qsvg4.dll		${INSTDIR}/lib/plugins/imageformats/
+
+
+
+# stripping
+strip -v ${INSTDIR}/*.exe
+strip -v ${INSTDIR}/*.dll
+strip -v ${INSTDIR}/lib/plugins/iconengines/*.dll
+strip -v ${INSTDIR}/lib/plugins/imageformats/*.dll
