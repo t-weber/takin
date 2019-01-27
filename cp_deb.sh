ca661e78463f42f9aaa65b916df61aa16b87f520
Tobias Weber <tobias.weber@tum.de>
2019-01-27 11:11:16 +0100

    version 1.6.2

--- cp_deb.sh
+++ cp_deb.sh
@@ -19,7 +19,7 @@ mkdir -p ${INSTDIR}/DEBIAN
 
 
 # control file
-echo -e "Package: takin\nVersion: 1.6.1" > ${INSTDIR}/DEBIAN/control
+echo -e "Package: takin\nVersion: 1.6.2" > ${INSTDIR}/DEBIAN/control
 echo -e "Description: inelastic neutron scattering software" >> ${INSTDIR}/DEBIAN/control
 echo -e "Maintainer: n/a" >> ${INSTDIR}/DEBIAN/control
 echo -e "Architecture: $(dpkg --print-architecture)" >> ${INSTDIR}/DEBIAN/control
